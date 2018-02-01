f3bed24c0ecd1aa580105147f76acf6271d37491
Martin Geissmann <martin.geissmann@gmail.com>
2018-02-01 18:13:04 +0100

    checking if commit works

--- quantmod_wallstreet.R
+++ quantmod_wallstreet.R
@@ -1,28 +1,28 @@
 library(quantmod)
 
-# display a simple bar chart
-getSymbols(c("AMZN"))
-barChart(AMZN,theme='white.mono',bar.type='hlc') 
- 
-# display a complex chart
-getSymbols(c("^GSPC"))
-chartSeries(GSPC, subset='last 3 months')
-addBBands(n = 20, sd = 2, ma = "SMA", draw = 'bands', on = -1)
 
-# get market data for all symbols making up the Nasdaq 100 index
-Nasdaq100_Symbols <- c("AAPL", "ADBE", "ADI", "ADP", "ADSK", "AKAM", "ALTR", "ALXN", 
-"AMAT", "AMGN", "AMZN", "ATVI", "AVGO", "BBBY", "BIDU", "BIIB", 
-"BRCM", "CA", "CELG", "CERN", "CHKP", "CHRW", "CHTR", "CMCSA", 
-"COST", "CSCO", "CTRX", "CTSH", "CTXS", "DISCA", "DISCK", "DISH", 
-"DLTR", "DTV", "EBAY", "EQIX", "ESRX", "EXPD", "EXPE", "FAST", 
-"FB", "FFIV", "FISV", "FOXA", "GILD", "GMCR", "GOOG", "GOOGL", 
-"GRMN", "HSIC", "ILMN", "INTC", "INTU", "ISRG", "KLAC", "KRFT", 
-"LBTYA", "LLTC", "LMCA", "LMCK", "LVNTA", "MAR", "MAT", "MDLZ", 
-"MNST", "MSFT", "MU", "MXIM", "MYL", "NFLX", "NTAP", "NVDA", 
-"NXPI", "ORLY", "PAYX", "PCAR", "PCLN", "QCOM", "QVCA", "REGN", 
-"ROST", "SBAC", "SBUX", "SIAL", "SIRI", "SNDK", "SPLS", "SRCL", 
-"STX", "SYMC", "TRIP", "TSCO", "TSLA", "TXN", "VIAB", "VIP", 
-"VOD", "VRSK", "VRTX", "WDC", "WFM", "WYNN", "XLNX", "YHOO")
+# mkt data SPI
+SPI_Symbols <- c("ABBN.VX", "ADXN.SW", "ADENE.SW", "ADVN.SW", "AEVS.SW", "AIRE.SW", "AIRN.SW", 
+  "ALLN.SW", "ALSN.SW", "AMS.SW", "APGN.SW", "ARBN.SW", "ARON.SW", "ARYN.VX", "ASCN.SW", "AUTN.SW", "BANB.SW", "BALNE.SW", 
+  "BPDG.SW", "BARN.SW", "BLKB.SW", "BSLN.SW", "BSKP.SW", "BCGE.SW", "BCJ.SW", "BCVN.SW", "BEKN.SW", "BEAN.SW", "BELL.SW", 
+  "BBN.SW", "BLIN.SW", "BC.SW", "LINN.SW", "BKW.SW", "BOBNN.SW", "BOSN.SW", "BUCN.SW", "BCHN.SW", "BRKN.SW", "BVZN.SW", 
+  "CALN.SW", "SKIN.SW", "CMBN.SW", "CPGN.SW", "CIE.SW", "CICN.SW", "CFT.SW", "CLN.VX", "CLTN.SW", "COTN.SW","CON.SW", 
+  "COPN.SW", "CPHN.SW", "CLXN.SW", "CSGN.VX", "DAE.SW", "DKSH.SW", "DOKA.SW", "DUFN.VX", "ESUN.SW", "EFGN.SW", "ELMN.SW", 
+  "EMMN.SW", "EMSN.SW", "EVE.SW", "FTON.SW", "FI.SW", "FHZN.SW", "FORN.SW", "GALE.SW", "GAM.SW", "GAV.SW", "GEBN.VX", 
+  "GIVN.VX", "GLKBN.SW", "GMI.SW", "GBMN.SW", "GRKP.SW", "GUR.SW", "HELN.SW", "HIAG.SW", "HLEE.SW", "HOCN.SW", "HUBN.SW", 
+  "HUE.SW", "HBLN.SW", "IDIA.SW", "IMPN.SW", "IFCN.SW", "INRN.SW", "ISN.SW", "IREN.SW", "VBSN.SW", "BAER.VX", "JFN.SW", 
+  "KARN.SW", "KOMN.SW", "KTMI.SW", "KUD.SW", "KNIN.VX", "KURN.SW", "LHN.VX", "LAND.SW", "LMN.SW", "LECN.SW", "LEHN.SW", 
+  "LEON.SW", "LLBN.SW", "LISN.SW", "LISP.SW", "LOGN.VX", "LONN.VX", "LUMX.SW", "LUKN.SW", "MCHN.SW", "METN.SW", "MBTN.SW", 
+  "MIKN.SW", "MOZN.SW", "MOBN.SW", "MOLN.SW", "MYRN.SW", "NESN.VX", "NWRN.SW", "NOVN.VX", "OFN.SW", "OERL.SW", "ODHN.SW", 
+  "ORON.SW", "PWTN.SW", "PARG.SW", "PGHN.VX", "PEAN.SW", "PRFN.SW", "PEDU.SW", "PM.SW", "PLAN.SW", "PNHO.SW", "PEHN.SW", 
+  "PSPN.SW", "RLF.SW", "CFR.VX", "RIEN.SW", "ROG.VX", "HREN.SW", "SANN.SW", "SAHN.SW", "SCHN.SW", "SCHP.VX", "STRN.SW", 
+  "STLN.SW", "SWTQ.SW", "SFSN.SW", "SGSN.VX", "SFZN.SW", "SIK.VX", "SNBN.SW", "SOON.SW", "SPCE.SW", "SGKN.SW", "STGN.SW", 
+  "STMN.SW", "SUN.SW", "SRCG.SW", "UHR.VX", "UHRN.SW", "SFPN.SW", "SLHN.SW", "SPSN.SW", "SREN.VX", "SCMN.VX", "SQN.SW", 
+  "TAMN.SW", "TECN.SW", "TEMN.SW", "TKBP.SW", "TIBN.SW", "TOHN.SW", "UBXN.SW", "UBSG.VX", "VLRT.SW", "VATN.SW", "VALN.SW", 
+  "VARN.SW", "VACN.SW", "VAHN.SW", "VET.SW", "VIFN.VX", "VILN.SW", "ROL.SW", "VONN.SW", "VPBN.SW", "VZN.SW", "WKBN.SW", 
+  "WMN.SW", "WARN.SW", "WIHN.SW", "YPSN.SW", "ZEHN.SW", "ZUBN.SW", "ZUGN.SW", "ZG.SW", "ROSE.SW", "ZURN.VX", "ZWM.SW")
+  
+SPI_test <- c("ABBN.VX", "ADXN.SW", "ADENE.SW")
 getSymbols(Nasdaq100_Symbols)
 
 # merge them all together
