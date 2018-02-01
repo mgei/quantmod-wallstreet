5580d0c31a58a4248ee1949e40a917c4081880cc
Martin Geissmann <martin.geissmann@gmail.com>
2018-02-01 20:52:44 +0100

    changes for my own data

--- quantmod_wallstreet.R
+++ quantmod_wallstreet.R
@@ -1,6 +1,5 @@
 library(quantmod)
 
-
 # mkt data SPI
 SPI_Symbols <- c("ABBN.VX", "ADXN.SW", "ADENE.SW", "ADVN.SW", "AEVS.SW", "AIRE.SW", "AIRN.SW", 
   "ALLN.SW", "ALSN.SW", "AMS.SW", "APGN.SW", "ARBN.SW", "ARON.SW", "ARYN.VX", "ASCN.SW", "AUTN.SW", "BANB.SW", "BALNE.SW", 
@@ -23,84 +22,51 @@ SPI_Symbols <- c("ABBN.VX", "ADXN.SW", "ADENE.SW", "ADVN.SW", "AEVS.SW", "AIRE.S
   "WMN.SW", "WARN.SW", "WIHN.SW", "YPSN.SW", "ZEHN.SW", "ZUBN.SW", "ZUGN.SW", "ZG.SW", "ROSE.SW", "ZURN.VX", "ZWM.SW")
   
 SPI_test <- c("ABBN.VX", "ADXN.SW", "ADENE.SW")
-getSymbols(Nasdaq100_Symbols)
 
-# merge them all together
-nasdaq100 <- data.frame(as.xts(merge(AAPL, ADBE, ADI, ADP, ADSK, AKAM, 
-                ALTR, ALXN,AMAT, AMGN, AMZN, ATVI, AVGO, BBBY, BIDU, BIIB, 
-                 BRCM, CA, CELG, CERN, CHKP, CHRW, CHTR, CMCSA, 
-                 COST, CSCO, CTRX, CTSH, CTXS, DISCA, DISCK, DISH, 
-                 DLTR, DTV, EBAY, EQIX, ESRX, EXPD, EXPE, FAST, 
-                 FB, FFIV, FISV, FOXA, GILD, GMCR, GOOG, GOOGL, 
-                 GRMN, HSIC, ILMN, INTC, INTU, ISRG, KLAC, KRFT, 
-                 LBTYA, LLTC, LMCA, LMCK, LVNTA, MAR, MAT, MDLZ, 
-                 MNST, MSFT, MU, MXIM, MYL, NFLX, NTAP, NVDA, 
-                 NXPI, ORLY, PAYX, PCAR, PCLN, QCOM, QVCA, REGN, 
-                 ROST, SBAC, SBUX, SIAL, SIRI, SNDK, SPLS, SRCL, 
-                 STX, SYMC, TRIP, TSCO, TSLA, TXN, VIAB, VIP, 
-                 VOD, VRSK, VRTX, WDC, WFM, WYNN, XLNX, YHOO)))
-head(nasdaq100[,1:12],2)
+tickers <- SPI_test
+from = "2000-01-01"
+ntic <- length(tickers)
+for (i in 1:ntic) {
+  temp <- getSymbols(tickers[i], auto.assign = FALSE, from = from)
+  if (i == 1) {
+    data <- temp
+  } else {
+    data <- merge(data,temp)
+  }
+  rm(temp)
+  Sys.sleep(1)
+}
+data_df <- data.frame(data)
 
 # set outcome variable
-outcomeSymbol <- 'FISV.Volume'
+outcomeSymbol <- 'ABBN.VX.Volume'
 
 # shift outcome value to be on same line as predictors
-library(xts)
-nasdaq100 <- xts(nasdaq100,order.by=as.Date(rownames(nasdaq100)))
-nasdaq100 <- as.data.frame(merge(nasdaq100, lm1=lag(nasdaq100[,outcomeSymbol],-1)))
-nasdaq100$outcome <- ifelse(nasdaq100[,paste0(outcomeSymbol,'.1')] > nasdaq100[,outcomeSymbol], 1, 0)
+data_e <- merge(data, lm1 = lag(data[,outcomeSymbol],-1))
+data_e$outcome <- ifelse(data_e[,paste0(outcomeSymbol,".1")] > data_e[,paste0(outcomeSymbol)], 1, 0)
 
-# remove shifted down volume field as we don't care by the value
-nasdaq100 <- nasdaq100[,!names(nasdaq100) %in% c(paste0(outcomeSymbol,'.1'))]
+data_e <- data_e[,-(ntic*6+1)]
 
-# cast date to true date and order in decreasing order
-nasdaq100$date <- as.Date(row.names(nasdaq100))
-nasdaq100 <- nasdaq100[order(as.Date(nasdaq100$date, "%m/%d/%Y"), decreasing = TRUE),]
+returns <- ROC(data_e)[,-ncol(data_e)]
+data_e <- merge(data_e, returns)
 
-# calculate all day differences and populate them on same row
-GetDiffDays <- function(objDF,days=c(10), offLimitsSymbols=c('outcome'), roundByScaler=3) {
-        # needs to be sorted by date in decreasing order
-        ind <- sapply(objDF, is.numeric)
-        for (sym in names(objDF)[ind]) {
-                if (!sym %in% offLimitsSymbols) {
-                        print(paste('*********', sym))
-                        objDF[,sym] <- round(scale(objDF[,sym]),roundByScaler)
-                        
-                        print(paste('theColName', sym))
-                        for (day in days) {
-                                objDF[paste0(sym,'_',day)] <- c(diff(objDF[,sym],lag = day),rep(x=0,day)) * -1
-                        }
-                }
-        }
-        return (objDF)
-}
+# data_e$date <- as.Date(index(data_e))
 
-# call the function with the following differences
-nasdaq100 <- GetDiffDays(nasdaq100, days=c(1,2,3,4,5,10,20), offLimitsSymbols=c('outcome'), roundByScaler=2)
+data_e <- na.locf(data_e)
 
-# drop most recent entry as we don't have an outcome
-nasdaq100 <- nasdaq100[2:nrow(nasdaq100),]
+data_e <- data_e["2015/"]
 
-# take a peek at YHOO features:
-dput(names(nasdaq100)[grepl('YHOO.',names(nasdaq100))])
-
-# well use POSIXlt to add day of the week, day of the month, day of the year
-nasdaq100$wday <- as.POSIXlt(nasdaq100$date)$wday
-nasdaq100$yday <- as.POSIXlt(nasdaq100$date)$mday
-nasdaq100$mon<- as.POSIXlt(nasdaq100$date)$mon
- 
-# remove date field and shuffle data frame
-nasdaq100 <- subset(nasdaq100, select=-c(date))
-nasdaq100 <- nasdaq100[sample(nrow(nasdaq100)),]
+data_r <- data.frame(data_e)
+data_r <- data_r[sample(nrow(data_r)),]
  
 # let's model
 library(xgboost)
-predictorNames <- names(nasdaq100)[names(nasdaq100) != 'outcome']
+predictorNames <- names(data_r)[names(data_r) != 'outcome']
  
 set.seed(1234)
-split <- sample(nrow(nasdaq100), floor(0.7*nrow(nasdaq100)))
-train <-nasdaq100[split,]
-test <- nasdaq100[-split,]
+split <- sample(nrow(data_r), floor(0.7*nrow(data_r)))
+train <-data_r[split,]
+test <- data_r[-split,]
  
 bst <- xgboost(data = as.matrix(train[,predictorNames]),
                label = train$outcome,
@@ -109,7 +75,7 @@ bst <- xgboost(data = as.matrix(train[,predictorNames]),
                gamma = 50, 
                nround = 50,
                colsample_bytree = 0.1,
-               subsample = 8.6,
+               subsample = 1, #8.6
                objective="binary:logistic")
  
 predictions <- predict(bst, as.matrix(test[,predictorNames]), outputmargin=TRUE)
